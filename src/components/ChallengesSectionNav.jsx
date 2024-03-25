73d6045395175b307d911a622c2c869b821a0d92
Your Name <you@example.com>
2024-03-25 17:15:33 +0600

    backup

---
+++ src/components/ChallengesSectionNav.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+//modules
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { showFilterOption } from '../store/slices/challengesSearchFormSlice'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+//components
+import Icon from 'react-inlinesvg'
+import FilterOption from './FilterOption'
+//svg
+import searchSvg from '../assets/svg/search.svg'
+import filterSvg from '../assets/svg/filter.svg'
+//css
+import '../assets/css/challenges-section-nav.css'
+function ChallengesSectionNav() {
+  const dispatch = useDispatch()
+  const isFilterOptionActive = useSelector(state => state.challengesSearchFormSlice.isFilterOptionActive)   
+
+  return (
+    <nav className=" flex-rw">
+        <Link to="/create-new-challenge">
+      <button className="create-challenge-button button positive flex-rw center button">
+        <span className="svgCont">
+          <Icon src={plusSvg}/>
+        </span>
+        <b>Create new</b>
+      </button>
+      </Link>
+
+      <div className="challenges-search-form flex-rw center left">
+            <input type="text" placeholder="Type here to search.." />
+            <div className="button-container flex-rw center">
+                <button className="search-button flex-rw center  button" type="submit">
+                    <Icon src={searchSvg} />
+                </button>
+                <button className="filter-button flex-rw center positive button" onClick={() => dispatch(showFilterOption())}>
+                    <Icon src={filterSvg} />
+                </button>
+            </div>
+        </div>
+                    {
+         isFilterOptionActive &&  <FilterOption />
+            }
+      </nav>
+  )
+}
+
+export default ChallengesSectionNav
--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -1,18 +1,11 @@
 //Modules
 import React, { useState } from "react";
 import dummyRecentChallengesData from '../data/recentChallengesData.json'
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 //components
-import ChallengesSearchForm from "../components/ChallengesSearchForm";
-import FilterOption from "../components/FilterOption";
+import ChallengesSectionNav from "../components/ChallengesSectionNav";
 import ChallengeCardsWrapper from "../components/ChallengeCardsWrapper";
-import Icon from 'react-inlinesvg'
-//svg
-import searchSvg from '../assets/svg/search.svg'
-import filterSvg from '../assets/svg/filter.svg'
-import plusSvg from '../assets/svg/plus(1).svg'
+
 //css
 import '../assets/css/challenges.css';
 
@@ -20,32 +13,15 @@ function Challenges() {
 
   const [challengesData, setChallengesData] = useState(dummyRecentChallengesData)
 
-  const isFilterOptionActive = useSelector(state => state.challengesSearchFormSlice.isFilterOptionActive)   
 
   return (
     <section className="challenges-section flex-cm">
-      <nav className="flex-rw">
-        <span className="total-challenges">Total: <span className="data">100</span></span>
-            {/* <div className="search-form-wrapper">
-            </div>   */}
-
-              <ChallengesSearchForm/>
-            {
-         isFilterOptionActive &&  <FilterOption />
-            }
-      </nav>
+     
+      <ChallengesSectionNav />
 
       <ChallengeCardsWrapper data={challengesData} />
 
-     <Link to="/create-new-challenge">
-      <button className="create-challenge-button positive flex-rw center button">
-        <span className="svgCont">
-          <Icon src={plusSvg}/>
-        </span>
-
-        <b>New</b>
-      </button>
-      </Link>
+   
     </section>
 
 
