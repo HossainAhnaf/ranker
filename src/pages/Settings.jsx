f22cfaec85128ebd01330d704a28394f418a2222
Your Name <you@example.com>
2024-03-29 17:30:48 +0600

    backup

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 import Icon from 'react-inlinesvg'
 //svg
 import profileSvg from '../assets/svg/profile.svg'
+import passwordSvg from '../assets/svg/password.svg'
+import notificationSvg from '../assets/svg/notification.svg'
 //css
 import '../assets/css/settings.css'
 function Settings() {
   return (
-    <section className='settings-section flex-rw center'>
+    <section className='settings-section flex-cm center'>
       <div className="settings-group">
        <div className="heading flex-cm">
        <h1 className="title">Account</h1>
@@ -23,10 +25,26 @@ function Settings() {
           </div>
           <div className="setting flex-rw">
             <div className="svgCont">
-              <Icon src={profileSvg} />
+              <Icon src={passwordSvg} />
             </div>
             <b>Password</b>
           </div>
+        </div>
+      </div>
+
+      <div className="settings-group">
+       <div className="heading flex-cm">
+       <h1 className="title">Preferences</h1>
+        <small className="description">Customize your experience</small>
+        </div> 
+        <div className="settings-wrapper">       
+          <div className="setting flex-rw">
+            <div className="svgCont">
+              <Icon src={notificationSvg} />
+            </div>
+            <b>Notifications</b>
+          </div>
+
         </div>
       </div>
     </section>
