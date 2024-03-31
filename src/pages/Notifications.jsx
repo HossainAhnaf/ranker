c1a86cc8ec7fdc6d7fdd8a67342c6a57999b783f
Your Name <you@example.com>
2024-04-01 00:31:43 +0600

    backup

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -13,13 +13,37 @@ import removeSvg from '../assets/svg/remove.svg'
 import removeAllSvg from '../assets/svg/remove-all.svg'
 //css
 import '../assets/css/notifications.css'
+import { Link } from 'react-router-dom'
 function Notifications() {
   const [notificationsData, setNotificationsData] = useState(fakeNotificationsData)
-   const moreButtonClickHandler = ({currentTarget}) => {
-     currentTarget.classList.toggle('active')
-   } 
+
+  const moreButtonClickHandler = ({ currentTarget }) => {
+    currentTarget.classList.toggle('active')
+  }
   return (
     <section className="notifications-section flex-cm center">
+      <nav className="flex-rw">
+        <div className="filter-buttons-wrapper flex-rw">
+          <button className="active  button">All</button>
+          <button className="button">Unread</button>
+        </div>
+        <div className="secondary flex-rw">
+          <button className="more-button button svgCont" onClick={moreButtonClickHandler}>
+            <div className="more-options-wrapper">
+              <button className="option button">
+                <div className="svgCont">
+                  <Icon src={tickSvg} />
+                </div>
+                <p> Mark all as read </p>
+              </button>
+            </div>
+              <Icon src={moreSvg} />
+          </button>
+          {/* <Link className="see-all-button button" to="/notifications">See all</Link> */}
+        </div>
+      </nav>
+
+
       {
         Object.keys(notificationsData).map((key, groupIndex) => {
           return (
@@ -28,51 +52,51 @@ function Notifications() {
               <div className="notifications-wrapper flex-cm center">
                 {
                   notificationsData[key].map(({ title, description, type, links }, index) => {
-                 return(
-                    <div className={`notification ${type} flex-rw `} key={index}>
-                      <div className="icon-wrapper">
-                        <div className="svgCont">
-                          {type === 'progress' && <Icon src={growthSvg} />}
-                          {type === 'security' && <Icon src={securitySvg} />}
-                          {type === 'announcement' && <Icon src={handMikeSvg} />}
-                        </div>
-                      </div>
-                      <div className="message-wrapper flex-cm">
-                        <b className="title">{title}</b>
-                        <small className="description">{description}</small>
-                        <div className="links-wrapper flex-rw">
-                          {links.map(({ title, url }, linkIndex) => {
-                            return (
-                              <a href={url} className="link button" key={linkIndex}>{title}</a>
-                            )
-                          })}
-                        </div>
-                      </div>
-                      <button className="more-button button svgCont" onClick={moreButtonClickHandler}>
-                        <Icon src={moreSvg} /> 
-                      </button> 
-                      <div className="more-options-wrapper">
-                        <button className="option button">
+                    return (
+                      <div className={`notification ${type} flex-rw `} key={index}>
+                        <div className="icon-wrapper">
                           <div className="svgCont">
-                            <Icon src={tickSvg} />
+                            {type === 'progress' && <Icon src={growthSvg} />}
+                            {type === 'security' && <Icon src={securitySvg} />}
+                            {type === 'announcement' && <Icon src={handMikeSvg} />}
                           </div>
-                          <p> Mark as read </p>
+                        </div>
+                        <div className="message-wrapper flex-cm">
+                          <b className="title">{title}</b>
+                          <small className="description">{description}</small>
+                          <div className="links-wrapper flex-rw">
+                            {links.map(({ title, url }, linkIndex) => {
+                              return (
+                                <a href={url} className="link button" key={linkIndex}>{title}</a>
+                              )
+                            })}
+                          </div>
+                        </div>
+                        <button className="more-button button svgCont" onClick={moreButtonClickHandler}>
+                          <Icon src={moreSvg} />
+                        </button>
+                        <div className="more-options-wrapper">
+                          <button className="option button">
+                            <div className="svgCont">
+                              <Icon src={tickSvg} />
+                            </div>
+                            <p> Mark as read </p>
                           </button>
                           <button className="option button">
-                          <div className="svgCont">
-                            <Icon src={removeSvg} />
-                          </div>
-                          <p>Remove this notification</p>
+                            <div className="svgCont">
+                              <Icon src={removeSvg} />
+                            </div>
+                            <p>Remove this notification</p>
                           </button>
                           <button className="option button">
-                          <div className="svgCont">
-                            <Icon src={removeAllSvg} />
-                          </div>
-                          <p>Turn of these notifications</p>
+                            <div className="svgCont">
+                              <Icon src={removeAllSvg} />
+                            </div>
+                            <p>Turn of these notifications</p>
                           </button>
+                        </div>
                       </div>
-                    </div>
-                 )
+                    )
                   })
 
                 }
