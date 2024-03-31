f9bfab993ad83f29bed049c2a0730c4169ad9d16
Your Name <you@example.com>
2024-04-01 01:52:40 +0600

    backup

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,6 +1,6 @@
 //modules
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import fakeNotificationsData from '../data/notificationsData.json'
 //components
 import Icon from 'react-inlinesvg'
@@ -13,11 +13,12 @@ import moreSvg from '../assets/svg/more.svg'
 import tickSvg from '../assets/svg/tick(1).svg'
 import removeSvg from '../assets/svg/remove.svg'
 import removeAllSvg from '../assets/svg/remove-all.svg'
+
 //css
 import '../assets/css/notifications.css'
 function Notifications() {
   const [notificationsData, setNotificationsData] = useState(fakeNotificationsData)
-
+  const navigate = useNavigate()
   const moreButtonClickHandler = ({ currentTarget }) => {
     currentTarget.classList.toggle('active')
   }
@@ -58,9 +59,9 @@ function Notifications() {
               <b className="date">{key}</b>
               <div className="notifications-wrapper flex-cm center">
                 {
-                  notificationsData[key].map(({ title, description, type, links }, index) => {
+                  notificationsData[key].map(({ title, description, type, url,isReaded }, index) => {
                     return (
-                      <div className={`notification ${type} flex-rw `} key={index}>
+                      <Link to={url} className={`notification ${type} ${isReaded ? 'readed' : ''} flex-rw `}   key={index} >
                         <div className="icon-wrapper">
                           <div className="svgCont">
                             {type === 'progress' && <Icon src={growthSvg} />}
@@ -71,13 +72,7 @@ function Notifications() {
                         <div className="message-wrapper flex-cm">
                           <b className="title">{title}</b>
                           <small className="description">{description}</small>
-                          <div className="links-wrapper flex-rw">
-                            {links.map(({ title, url }, linkIndex) => {
-                              return (
-                                <a href={url} className="link button" key={linkIndex}>{title}</a>
-                              )
-                            })}
-                          </div>
+                          
                         </div>
                         <button className="more-button button svgCont" onClick={moreButtonClickHandler}>
                           <Icon src={moreSvg} />
@@ -102,7 +97,7 @@ function Notifications() {
                             <p>Turn of these notifications</p>
                           </button>
                         </div>
-                      </div>
+                      </Link>
                     )
                   })
 
