89b803c5b5a4535d80e4bb90a85bbaf791e77c31
Your Name <you@example.com>
2024-04-04 19:59:17 +0600

    backup

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -5,7 +5,7 @@ import defaultAvatar from '../assets/img/user.jpeg'
 import '../assets/css/user-avatar.css'
 const UserAvatar = forwardRef(function UserAvatar({className,avatar,status,level},ref) {
   return (
-    <div className={`user-avatar ${className} ${status.toLowerCase()} imgCont logoCont`} ref={ref} >
+    <div className={`user-avatar ${className ? className : ''} ${status.toLowerCase()} imgCont logoCont`} ref={ref} >
     <img src={avatar || defaultAvatar} />
      <span className="level-badge">{level}</span>
      </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,7 @@ import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
 
 //fake
-import {singleUser} from '../data/usersData.json'
+import { singleUser } from '../data/usersData.json'
 //components
 import ProfileEditForm from '../components/ProfileEditForm/ProfileEditForm';
 import UserAvatar from '../components/UserAvatar';
@@ -17,63 +17,52 @@ import Icon from 'react-inlinesvg';
 import pencilSvg from '../assets/svg/pencil.svg';
 //css
 import '../assets/css/profile.css';
+import '../assets/css/mobile-large/profile.css';
 import '../assets/css/tablet/profile.css';
 
 function Profile() {
-   const params = useParams()
-  const {username,name,avatar,rank,status,challenges,passed,failed,level,xp} = useSelector(state=>state.userSlice)
-  
+  const params = useParams()
+  const { username, name, avatar, rank, status, challenges, passed, failed, level, xp } = useSelector(state => state.userSlice)
+
   const isAuthor = (username === params.username || params.username === 'me')
- 
-   const [isProfileEditFormVisible, setIsProfileEditFormVisible] = useState(false)
 
-  const [challengesData, setChallengesData] = useState(dummyRecentChallengesData) 
+  const [isProfileEditFormVisible, setIsProfileEditFormVisible] = useState(false)
 
-  const [authorUsername,setAuthorUsername] = useState(username)
-  const [authorName,setAuthorName] = useState(name)
-  const [authorAvatar,setAuthorAvatar] = useState(avatar)
-  const [authorRank,setAuthorRank] = useState(rank)
-  const [authorStatus,setAuthorStatus] = useState(status)
-  const [authorChallenges,setAuthorChallenges] = useState(challenges)
-  const [authorPassed,setAuthorPassed] = useState(passed)
-  const [authorFailed,setAuthorFailed] = useState(failed)
-  const [authorLevel,setAuthorLevel] = useState(level)
-  const [authorXp,setAuthorXp] = useState(xp)   
+  const [challengesData, setChallengesData] = useState(dummyRecentChallengesData)
+
+  const [authorUsername, setAuthorUsername] = useState(username)
+  const [authorName, setAuthorName] = useState(name)
+  const [authorAvatar, setAuthorAvatar] = useState(avatar)
+  const [authorRank, setAuthorRank] = useState(rank)
+  const [authorStatus, setAuthorStatus] = useState(status)
+  const [authorChallenges, setAuthorChallenges] = useState(challenges)
+  const [authorPassed, setAuthorPassed] = useState(passed)
+  const [authorFailed, setAuthorFailed] = useState(failed)
+  const [authorLevel, setAuthorLevel] = useState(level)
+  const [authorXp, setAuthorXp] = useState(xp)
   const [progressParcentage, setProgressParcentage] = useState(0)
 
- 
 
- 
-   const fetchChallengesData = () => {
+
+
+  const fetchChallengesData = () => {
     // fetch('../data/recentChallengesData.json')
     // .then(res=>res.json())
     // .then(setChallengesData)
   }
 
-  const numberTypeInfoIncreaceHandler = (challenges,passed,failed,level,xp) => {
-   useSetValueFromStart(0,challenges,setAuthorChallenges)
-    useSetValueFromStart(0,passed, setAuthorPassed)
-    useSetValueFromStart(0,failed, setAuthorFailed)
-    useSetValueFromStart(0,level,setAuthorLevel)
-    useSetValueFromStart(0,xp,setAuthorXp)
-    useSetValueFromStart(0, (xp / 1000) * 100,setProgressParcentage)
+  const numberTypeInfoIncreaceHandler = (challenges, passed, failed, level, xp) => {
+    useSetValueFromStart(0, challenges, setAuthorChallenges)
+    useSetValueFromStart(0, passed, setAuthorPassed)
+    useSetValueFromStart(0, failed, setAuthorFailed)
+    useSetValueFromStart(0, level, setAuthorLevel)
+    useSetValueFromStart(0, xp, setAuthorXp)
+    useSetValueFromStart(0, (xp / 1000) * 100, setProgressParcentage)
   }
 
   const fetchProfileData = () => {
     if (!isAuthor) {
-     const {username,name,avatar,rank,status,challenges,passed,failed,level,xp} = singleUser[params.username]
-     setAuthorUsername(username)
-     setAuthorName(name)
-     setAuthorAvatar(avatar)
-     setAuthorRank(rank)
-     setAuthorStatus(status)
-     setAuthorChallenges(challenges)
-     setAuthorPassed(passed)
-     setAuthorFailed(failed)
-     setAuthorLevel(level)
-     setAuthorXp(xp)
-     numberTypeInfoIncreaceHandler(challenges,passed,failed,level,xp)
-    } else{
+      const { username, name, avatar, rank, status, challenges, passed, failed, level, xp } = singleUser[params.username]
       setAuthorUsername(username)
       setAuthorName(name)
       setAuthorAvatar(avatar)
@@ -84,62 +73,84 @@ function Profile() {
       setAuthorFailed(failed)
       setAuthorLevel(level)
       setAuthorXp(xp)
-      numberTypeInfoIncreaceHandler(challenges,passed,failed,level,xp)
+      numberTypeInfoIncreaceHandler(challenges, passed, failed, level, xp)
+    } else {
+      setAuthorUsername(username)
+      setAuthorName(name)
+      setAuthorAvatar(avatar)
+      setAuthorRank(rank)
+      setAuthorStatus(status)
+      setAuthorChallenges(challenges)
+      setAuthorPassed(passed)
+      setAuthorFailed(failed)
+      setAuthorLevel(level)
+      setAuthorXp(xp)
+      numberTypeInfoIncreaceHandler(challenges, passed, failed, level, xp)
     }
-   }
- 
-  useEffect(() => {
-   fetchProfileData()
-   if (isAuthor) 
-   fetchChallengesData()
-  }, [params])
-  
+  }
 
- 
+  useEffect(() => {
+    fetchProfileData()
+    if (isAuthor)
+      fetchChallengesData()
+  }, [params])
+
+
+
   return (
     <div className="profile">
       <section className="info-section flex-cm center">
         <div className="primary flex-cm center">
-         <div className="top-items-wrapper flex-rw"> 
-          <p className='username'>@{authorUsername}</p>
+
+          <div className="user-primary-info-wrapper flex-rw">
+            <div className="image-side">
+              <UserAvatar avatar={authorAvatar} status={authorStatus} level={authorLevel} />
+            </div>
+            <div className="text-side flex-cm">
+              <h3 className="name">{authorName}</h3>
+              <small className='username'>@{authorUsername}</small>
+              <small className={`status ${authorStatus.toLowerCase()}`}>{authorStatus}</small>
+              <p className="rank">Rank {rank}</p>
+            </div>
+
+          </div>
           {isAuthor &&
-          <button className="profile-edit-button flex-rw center button" onClick={() => setIsProfileEditFormVisible(true)}>
+          <button className="profile-edit-button positive button flex-rw center button" onClick={() => setIsProfileEditFormVisible(true)}>
             <div className="svgCont flex-rw center">
             <Icon src={pencilSvg} />   
             </div> 
-            <span>Edit</span>
+            <span>Edit Profile</span>
           </button>
            }
-          </div>
-          <UserAvatar avatar={authorAvatar} status={authorStatus} level={authorLevel} />
-          <h3 className="name">{authorName}</h3>
         </div>
-        <Info name='Rank' value={authorRank} />
-        <Info name='Status' value={authorStatus} />
-        <Info name='Challenges' value={authorChallenges} />
-        <Info name='Passed' value={authorPassed} />
-        <Info name='Failed' value={authorFailed} />
+        <hr />
+        <div className="challenges-activity flex-cm">
+          <h3 className="title">Challenges Activity</h3>
+         <Info name='Total' value={authorChallenges} />
+         <Info name='Passed' value={authorPassed} />
+         <Info name='Failed' value={authorFailed} />
+         </div>
         <br />
         <hr />
         <br />
-   
-         <div role='progressbar' style={{"--value": progressParcentage}}></div>
-                
+
+        <div role='progressbar' style={{ "--value": progressParcentage }}></div>
+
         <Info name='Level' value={`${authorLevel} / 10`} />
         <Info name='Xp' value={`${authorXp} / 1000`} />
       </section>
-     {
-      isProfileEditFormVisible &&
-      <ProfileEditForm  setIsProfileEditFormVisible={setIsProfileEditFormVisible}/>
-     }
-     {
-    isAuthor &&
-      <section className="recent-challenges flex-cm">
-        <h3 className='title'>Recent Challenges</h3>
-        <ChallengeCardsWrapper data={challengesData} shortDesc={true} />
-        <button className="see-all-button button positive">See all</button>
-      </section>
-     }
+      {
+        isProfileEditFormVisible &&
+        <ProfileEditForm setIsProfileEditFormVisible={setIsProfileEditFormVisible} />
+      }
+      {
+        isAuthor &&
+        <section className="recent-challenges flex-cm">
+          <h3 className='title'>Recent Challenges</h3>
+          <ChallengeCardsWrapper data={challengesData} shortDesc={true} />
+          <button className="see-all-button button positive">See all</button>
+        </section>
+      }
     </div>
   )
 }
