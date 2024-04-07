84c2d190c901d0206555f868c33957bfc03b180c
Your Name <you@example.com>
2024-04-07 16:58:08 +0600

    backup

--- src/components/ChallengesSectionNav.jsx
+++ src/components/ChallengesSectionNav.jsx
@@ -19,40 +19,71 @@ function ChallengesSectionNav() {
 
   return (
     <nav className="challenges-section-nav flex-rw" >
+      <div className="total-challenges-data-wrapper flex-rw">
+        <span className="data flex-rw center">
+          <b className="name">Total</b>
+          <span className="value">10</span>
+        </span>
+        <span className="data flex-rw center">
+          <b className="name easy">Easy</b>
+          <span className="value">500</span>
+        </span>
+        <span className="data flex-rw center">
+          <b className="name medium">Medium</b>
+          <span className="value">4</span>
+        </span>
+        <span className="data flex-rw center">
+          <b className="name hard">Hard</b>
+          <span className="value">1</span>
+        </span>
+      </div>
       <MenuButton label="Difficulty" optionsType="button-type">
-      <span className="option">Easy
-       <Icon src={tickSvg}/>
-      </span>
-      <span className="option">Medium
-       <Icon src={tickSvg}/>
-      </span>
-      <span className="option">Hard
-       <Icon src={tickSvg}/>
-      </span>
-     </MenuButton> 
+        <span className="option easy">Easy
+          <Icon src={tickSvg} />
+        </span>
+        <span className="option medium">Medium
+          <Icon src={tickSvg} />
+        </span>
+        <span className="option hard">Hard
+          <Icon src={tickSvg} />
+        </span>
+      </MenuButton>
 
-     <MenuButton  label="Sort by" optionsType="button-type">
-      <span className="option selected">
-       Serial
-       <Icon src={tickSvg}/>
-      </span>
-      <span className="option">
-      Difficulty
-       <Icon src={tickSvg}/>
-      </span>      
-     </MenuButton> 
+      <MenuButton label="Sort by" optionsType="button-type">
+        <span className="option selected">
+          Serial
+          <Icon src={tickSvg} />
+        </span>
+        <span className="option">
+          Difficulty
+          <Icon src={tickSvg} />
+        </span>
+      </MenuButton>
 
-     <MenuButton label="Features" optionsType="checkbox-type">
-      <label className="option ">
-       <input type="checkbox"  hidden />
-       <span>Due Date</span>
+      <MenuButton label="Features" optionsType="checkbox-type">
+        <label className="option ">
+          <input type="checkbox" hidden />
+          <span>Due Date</span>
 
-      </label>
-      <label className="option">
-       <input type="checkbox"  hidden/>
-       <span>Pinned</span>
-      </label>
-     </MenuButton> 
+        </label>
+        <label className="option">
+          <input type="checkbox" hidden />
+          <span>Pinned</span>
+        </label>
+      </MenuButton>
+      <div className="search-input-wrapper flex-rw center">
+        <span className="svgCont">
+          <Icon src={searchSvg} />
+        </span>
+        <input type="text" placeholder="Search challenges.." />
+      </div>
+     
+        <Link to="/create-new-challenge" className="create-challenge-button button positive flex-rw center">
+          <span className="svgCont">
+            <Icon src={plusSvg} />
+          </span>
+          <b>Create new</b>
+        </Link>
     </nav>
   )
 }
@@ -60,14 +91,7 @@ function ChallengesSectionNav() {
 
 export default ChallengesSectionNav
 
-// <Link to="/create-new-challenge">
-// <button className="create-challenge-button button positive flex-rw center button">
-//   <span className="svgCont">
-//     <Icon src={plusSvg}/>
-//   </span>
-//   <b>Create new</b>
-// </button>
-// </Link>
+
 
 // <div className="challenges-search-form flex-rw center left">
 //       <input type="text" placeholder="Type here to search.." />
