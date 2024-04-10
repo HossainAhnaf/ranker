43cf18773f01a8ee953016c18da1f60f74fa5e88
Your Name <you@example.com>
2024-04-10 21:40:25 +0600

    backup

--- src/pages/Settings/NotificationSettings.jsx
+++ src/pages/Settings/NotificationSettings.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function NotificationSettings() {
   return (
-    <div>
-      
+    <div className='outlet notification-settings'>
+      <div className="header flex-rw">
+        <div className="name">Notifications</div>
+        <div className="right flex-rw">
+          <b>Email</b>
+          <b>Site</b>
+        </div>
+      </div>
+      <div className="notification-category">
+       Anouncement
+      </div>
+      <div className="notification-type-container">
+      <div className="notification-type">
+ 
+      </div>
+      </div>
     </div>
   )
 }
