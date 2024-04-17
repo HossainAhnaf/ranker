1397acabf46f9eb0ffbe191ec61222c3868e8860
Your Name <you@example.com>
2024-04-17 20:56:15 +0600

    backup

--- src/pages/Accounts/Signin.jsx
+++ src/pages/Accounts/Signin.jsx
@@ -13,53 +13,54 @@ import linkedinSvg from '../../assets/svg/linkedin.svg';
 function Signin() {
   return (
     <form className="form signin-form">
-    <h2 className="heading">Sign in</h2>
-  
-  
-    <OutlinedCustomField  className="email" placeholder="Email"  required={true}>
-      <input type="email" placeholder=" "  required={true}/>
-    </OutlinedCustomField>
-    <small className="message">&nbsp;</small>
-   
-    <OutlinedCustomField  className="password" placeholder="Password"  required={true}>
-      <input type="password" placeholder=" "  required={true}/>
-    </OutlinedCustomField>
-    <small className="message">&nbsp;</small>
+      <h2 className="heading">Sign in</h2>
 
-    <div className="links-container flex-rw">
-     <Link className="button">Forgot password?</Link>
-    </div>
-    <button className="button positive" type="submit">Confirm</button>
-    <p className="link-wrapper">
-    Don't have an account? <Link to="../signup" className='button'>Sign Up Now</Link>
-    </p>
-   
-    <div className="or">
-          <span className="border"></span>
-          <p>Or</p>  
-        </div>
+
+      <OutlinedCustomField className="email" placeholder="Email" required={true}>
+        <input type="email" placeholder=" " required={true} />
+      </OutlinedCustomField>
+      <small className="message">&nbsp;</small>
+
+      <OutlinedCustomField className="password" placeholder="Password" required={true}>
+        <input type="password" placeholder=" " required={true} />
+      </OutlinedCustomField>
+      <small className="message">&nbsp;
+      </small>
+
+      <div className="links-container flex-rw">
+        <Link className="button">Forgot password?</Link>
+      </div>
+      <button className="button positive" type="submit">Confirm</button>
+      <p className="link-wrapper">
+        Don't have an account? <Link to="../signup" className='button'>Sign Up Now</Link>
+      </p>
+
+      <div className="or">
+        <span className="border"></span>
+        <p>Or</p>
+      </div>
 
       <div className="with-social-media-container flex-cm center">
         <p className="title">Continue with social media</p>
-         <div className="platforms-wrapper flex-rw">
-         <button className="platform button">
+        <div className="platforms-wrapper flex-rw">
+          <button className="platform button">
             <Icon src={facebookSvg} />
-          </button>  
+          </button>
           <button className="platform button">
             <Icon src={twitterSvg} />
-          </button>  
-    
+          </button>
+
           <button className="platform button">
             <Icon src={googleSvg} />
           </button>
           <button className="platform button">
             <Icon src={linkedinSvg} />
-          </button>  
-                   
-          </div> 
+          </button>
+
+        </div>
 
       </div>
-  </form>
+    </form>
   )
 }
 
--- src/pages/Accounts/Signup.jsx
+++ src/pages/Accounts/Signup.jsx
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom';
 
 //components
 import OutlinedCustomField from "../../components/OutlinedCustomField"
-
+import Icon from 'react-inlinesvg';
+//svg
+import facebookSvg from '../../assets/svg/facebook.svg';
+import twitterSvg from '../../assets/svg/twitter.svg';
+import googleSvg from '../../assets/svg/google.svg';
+import linkedinSvg from '../../assets/svg/linkedin.svg';
 function Signup() {
   return (
     <form className="form signup-form">
@@ -24,8 +29,35 @@ function Signup() {
       <input type="password" placeholder=" "  required={true}/>
     </OutlinedCustomField>
     <small className="message">&nbsp;</small>
-    <p className='already-have-account'><small>Already have a account? <Link  to="../signin">Sign in</Link></small></p>
     <button className="button positive" type="submit">Confirm</button>
+    <p className="link-wrapper">
+    Already have account? <Link to="../signin" className='button'>Sign In Now</Link>
+    </p>
+    <div className="or">
+        <span className="border"></span>
+        <p>Or</p>
+      </div>
+
+      <div className="with-social-media-container flex-cm center">
+        <p className="title">Continue with social media</p>
+        <div className="platforms-wrapper flex-rw">
+          <button className="platform button">
+            <Icon src={facebookSvg} />
+          </button>
+          <button className="platform button">
+            <Icon src={twitterSvg} />
+          </button>
+
+          <button className="platform button">
+            <Icon src={googleSvg} />
+          </button>
+          <button className="platform button">
+            <Icon src={linkedinSvg} />
+          </button>
+
+        </div>
+
+      </div>
   </form>
   )
 }
