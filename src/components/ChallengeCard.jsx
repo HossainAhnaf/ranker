fbf9521629417c1524c8454f3b8e186691fc0a2c
HossainAhnaf <138277497+HossainAhnaf@users.noreply.github.com>
2024-06-03 11:15:06 +0600

    Update ChallengeCard.jsx

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -13,10 +13,13 @@ import '../assets/css/mobile-large/challenge-card.css'
 
 function ChallengeCard({ title, description, difficulty, pinned, id }) {
   const [isDetailsActive,setIsDetailsActive] = React.useState(false)
+  const [progress,setProgress] = React.useState(0)
+
   const taskDivisitionClickHandler = ({currentTarget:checkbox})=>{
     checkbox.disabled = true
     checkbox.parentElement.classList.add("done")
-   
+    setProgress(progress + parseInt(checkbox.value))
+  
   }
   const detailsToggleButtonClickHandler = ({currentTarget})=>{
   setIsDetailsActive(prev=> !prev)
@@ -39,27 +42,27 @@ function ChallengeCard({ title, description, difficulty, pinned, id }) {
       <div className="task-divisitions-wrapper">
 
       <label  className="task-divisition ">
-          <input type="checkbox" onChange={taskDivisitionClickHandler}/>
+          <input type="checkbox" onChange={taskDivisitionClickHandler} value="50"/>
           <small className="title">Lorem, ipsum dolor.</small>
-          <small className="value-percentage">+ 20%</small>
+          <small className="value-percentage" >+ 20%</small>
         </label>
           
         <label  className="task-divisition">
-          <input type="checkbox" onChange={taskDivisitionClickHandler}/>
+          <input type="checkbox" onChange={taskDivisitionClickHandler} value="50"/>
           <small className="title">Lorem, ipsum dolor.</small>
-          <small className="value-percentage">+ 20%</small>
+          <small className="value-percentage" >+ 20%</small>
         </label>
 
       </div>
-    </div>
+    </div> 
     <div className="footer">
       <div className="left">
         <button className={`details-toggle-button button"  ${isDetailsActive ? 'active' : ''}`} onClick={detailsToggleButtonClickHandler}>
         <svg fill="currentColor"  viewBox="0 0 36 36" version="1.1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>details-solid</title> <path d="M32,6H4A2,2,0,0,0,2,8V28a2,2,0,0,0,2,2H32a2,2,0,0,0,2-2V8A2,2,0,0,0,32,6ZM19,22H9a1,1,0,0,1,0-2H19a1,1,0,0,1,0,2Zm8-4H9a1,1,0,0,1,0-2H27a1,1,0,0,1,0,2Zm0-4H9a1,1,0,0,1,0-2H27a1,1,0,0,1,0,2Z" class="clr-i-solid clr-i-solid-path-1"></path> <rect x="0" y="0" width="36" height="36" fill-opacity="0"></rect> </g></svg>
         </button>
-        <div className="complete-progressbar">
-          <span className="percent-text">0%</span>
-          <div className="progress"></div>
+        <div className={`complete-progressbar ${isDetailsActive ? 'active' : ''}`}>
+          <span className="percent-text">{progress}%</span>
+          <div style={{width:`${progress}%`}} className="progress"></div>
         </div>
       </div>
       <div className="right flex-rw center left"> 
